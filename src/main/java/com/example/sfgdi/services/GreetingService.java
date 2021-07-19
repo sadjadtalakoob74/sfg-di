0270f4b88fe3c35264779674515f39a01491ae22
Sadjad <sadjadtalakob74@gmail.com>
2021-07-19 10:29:33 +0400

    Spring Injection

---
+++ src/main/java/com/example/sfgdi/services/GreetingService.java
@@ -0,0 +1,9 @@
+package com.example.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+
+public interface GreetingService {
+
+    String sayGreeting();
+}
---
+++ src/test/java/com/example/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.sfgdi.controllers;
+
+import com.example.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
